b35c9bc9907508ec25ce432a33772df7eaa0d91f
Syed Aasim <aasimsyed398@gmail.com>
2025-10-21 18:46:43 +0530

    completed Track Player Setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, useColorScheme, View, Text } from 'react-native';
+
+
+
+
+function App() {
+
+  return (
+    <View>
+     <Text>Test</Text>
+    </View>
+  );
+}
+
+
+
+export default App;
